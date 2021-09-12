0a9f7a4ddf6cdc24a0b95e99123e2093ebd3c7c4
Nicole Bills <nbills@atlasresearch.us>
2021-09-12 19:45:38 -0400

    initial components

--- src/Quote.js
+++ src/Quote.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import useFetch from "./useFetch";
 
 export default function Quote() {
+    const {get} = useFetch("https://stoicquotesapi.com/v1/api/quotes/random")
+
+    const [quoteObj, setQuoteObj] = useState({})
+    const [quote, setQuote] = useState("")
+    const [author, setAuthor] = useState("")
+    const [reset, setReset] = useState(1)
+
+    useEffect( () => {
+        get()
+        .then(data => {console.log(response) 
+        setQuoteObj(response)
+
+        const {body, author, ...rest} = quoteObj;
+
+        setQuote(body);
+        setAuthor(author);
+        })
+        .catch(error => console.log(error))
+    }, [reset])
+
+    function handleButtonClick(){
+        setReset(Math.random())
+    }
+
     return <>
-        <p id="text">{quoteText}</p>
-        <p id="author">{quoteAuthor}</p>
+        <p id="text">{quote}</p>
+        <p id="author">{author}</p>
+        <button id="new-quote" onClick={()=>handleButtonClick}>New quote</button>
     </>
 }
