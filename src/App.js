db4d02e080d977d6d04fa6bf5ec682d5917d5f1f
Nicole Bills <nbills@atlasresearch.us>
2021-09-13 18:09:17 -0400

    pushing even though useState() isn't working as expected

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Quote from "./Quote.js"
 function App() {
   return (
     <div id="quote-box">
-      <p> </p>
       <Quote />
     </div>
   );
--- src/Quote.js
+++ src/Quote.js
@@ -2,41 +2,52 @@ import React, {useState, useEffect} from "react";
 import useFetch from "./useFetch";
 
 export default function Quote() {
-    const { get, loading } = useFetch("https://stoicquotesapi.com/v1/api/quotes/")
+    const { get } = useFetch("https://stoicquotesapi.com/v1/api/quotes/")
 
-    const [quoteObj, setQuoteObj] = useState({})
-    const [quote, setQuote] = useState()
-    const [quoteAuthor, setQuoteAuthor] = useState()
+    const [quote, setQuote] = useState(" ")
+    const [quoteAuthor, setQuoteAuthor] = useState(" ")
     const [reset, setReset] = useState(1)
+    const [tweetString, setTweetString] = useState()
 
     useEffect( () => {
         getQuote()
-    }, []); //this is not working - not sure why.
 
-    useEffect( () => {
-        getQuote()
     }, [reset]);
+
+    useEffect( () => {
+        console.log("Author after author updates: "+quoteAuthor)
+        console.log("Quote after author updates: "+quote)
+
+        if (quote !== undefined) {
+            
+            const text = '"'+quote.replace(/ /g, "%20")+'"%20-'+quoteAuthor.replace(/ /g, "%20")
+            setTweetString(text)
+            console.log("tweetString: " + tweetString)
+        }
+    }, [quoteAuthor])
 
     function getQuote(){
         get("random")
         .then(data => {
             console.log(data) 
-            setQuoteObj(data)
-
-            const {body, author, ...rest} = quoteObj;
+            const {body, author, ...rest} = data;
 
             setQuote(body);
             setQuoteAuthor(author);
+
+            console.log("body: " + body)
+            console.log("auth: "+ author)
         })
         .catch(error => console.log(error))
     }
 
-    function handleResetClick(){
-        setReset(Math.random())
+    function handleTweetClick() {
+        console.log("tweetString should be updated HERE: "+tweetString)
+        window.open("https://twitter.com/intent/tweet?text="+{tweetString}+"&via=Nicole_Janeway", "_blank");
     }
 
-    function handleTweetClick() {
-        window.open("https://twitter.com/intent/tweet", "_blank");
+    function handleResetClick(){
+        setReset(Math.random())
     }
 
     return <>
